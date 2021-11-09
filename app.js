26a3128402bb548789c0768a8a9eac089e6f3f0e
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-09 17:14:48 +0100

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const number = document.querySelectorAll('#number .num')
 const operation = document.querySelectorAll('#operation button')
 const display = document.querySelectorAll('#display p')
-console.log(operation)
+
 let total= 0;
 let firstNum= "";
 for (x of number) {
@@ -14,11 +14,9 @@ let operator = "";
 for (x of operation){
     x.addEventListener("click", (event)=>{
         operator = event.target.id
-        console.log(operator)
         display[0].innerText = firstNum + operator
         total = firstNum + operator
         firstNum = "";
-        console.log(total)
     })
 }
 const calcola = document.querySelector('#number .calcola')
