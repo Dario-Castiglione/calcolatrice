e3bc5e550fe26f965ac4bf3c64aa3f4ed46dcdba
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-09 18:22:31 +0100

    .

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ let total= 0;
 let firstNum= "";
 for (x of number) {
     x.addEventListener("click", (event)=>{
-        firstNum = firstNum.concat("" , event.target.id)
-        display[1].innerText = firstNum 
+        firstNum = firstNum.concat("" , event.target.id);
+        display[1].innerText = firstNum;
     })
 }
 let operator = "";
 for (x of operation){
     x.addEventListener("click", (event)=>{
-        operator = event.target.id
-        display[0].innerText = firstNum + operator
-        total = firstNum + operator
+        operator = event.target.id;
+        total = firstNum + operator;
+        display[0].innerText = total;
         firstNum = "";
     })
 }
-const calcola = document.querySelector('#number .calcola')
+const calcola = document.querySelector('#number .calcola');
 calcola.addEventListener('click',()=>{
     let  calc = total + firstNum;
-    display[0].innerText = calc
-    display[1].innerText= eval(calc)
+    display[0].innerText = calc;
+    display[1].innerText= eval(calc);
     firstNum = eval(calc);
-    calc = ""  
+    calc = "";
 })
